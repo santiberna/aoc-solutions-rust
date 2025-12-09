439f8ad186f778b4d357b791d40a4761500470ae
Santiago Bernardino <santibberna@gmail.com>
2025-12-09 10:04:33 +0100

    Update c9.rs

--- src/y2025/c9.rs
+++ src/y2025/c9.rs
@@ -34,14 +34,6 @@ const TEST2: &str = r#"0,0
 
 // (4, 4)
 
-fn within_range_exclusive(v: i64, a: i64, b: i64) -> bool {
-    (v > a && v < b) || (v < a && v > b)
-}
-
-fn within_range_inclusive(v: i64, a: i64, b: i64) -> bool {
-    (v >= a && v <= b) || (v <= a && v >= b)
-}
-
 fn polygon_lines(coords: &[IVec2]) -> (Vec<(IVec2, IVec2)>, Vec<(IVec2, IVec2)>) {
     let mut a = coords.chunks(2).map(|s| (s[0], s[1])).collect::<Vec<_>>();
     let mut b = {
@@ -83,108 +75,6 @@ fn inside_rects(p: &IVec2, rects: &[(IVec2, IVec2)]) -> bool {
     return false;
 }
 
-fn rect_no_edge_out(rect: &(IVec2, IVec2), h: &[(IVec2, IVec2)], v: &[(IVec2, IVec2)]) -> bool {
-    let ha = (
-        IVec2::new(rect.0.x, rect.0.y),
-        IVec2::new(rect.1.x, rect.0.y),
-    );
-    let hb = (
-        IVec2::new(rect.0.x, rect.1.y),
-        IVec2::new(rect.1.x, rect.1.y),
-    );
-
-    // dbg!(&ha);
-    // dbg!(&hb);
-
-    for v_intersect in v.iter() {
-        let vert_x = v_intersect.0.x;
-
-        //dbg!(v_intersect);
-        //dbg!(vert_x);
-        //dbg!(&ha);
-
-        if within_range_exclusive(vert_x, ha.0.x, ha.1.x)
-            && within_range_inclusive(ha.0.y, v_intersect.0.y, v_intersect.1.y)
-        {
-            let y = ha.0.y;
-            let (y1, y2) = if v_intersect.0.y < v_intersect.1.y {
-                (v_intersect.0.y, v_intersect.1.y)
-            } else {
-                (v_intersect.1.y, v_intersect.0.y)
-            };
-
-            if !(y == y2 && y >= y1) {
-                return false;
-            }
-        }
-
-        //dbg!(&hb);
-
-        if within_range_exclusive(vert_x, hb.0.x, hb.1.x)
-            && within_range_inclusive(hb.0.y, v_intersect.0.y, v_intersect.1.y)
-        {
-            let y = hb.0.y;
-            let (y1, y2) = if v_intersect.0.y < v_intersect.1.y {
-                (v_intersect.0.y, v_intersect.1.y)
-            } else {
-                (v_intersect.1.y, v_intersect.0.y)
-            };
-
-            if !(y == y2 && y <= y1) {
-                return false;
-            }
-        }
-    }
-
-    let va = (
-        IVec2::new(rect.0.x, rect.0.y),
-        IVec2::new(rect.0.x, rect.1.y),
-    );
-    let vb = (
-        IVec2::new(rect.1.x, rect.0.y),
-        IVec2::new(rect.1.x, rect.1.y),
-    );
-
-    // dbg!(&va);
-    // dbg!(&vb);
-
-    for h_intersect in h.iter() {
-        let hori_y = h_intersect.0.y;
-
-        if within_range_exclusive(hori_y, va.0.y, va.1.y)
-            && within_range_inclusive(va.0.x, h_intersect.0.x, h_intersect.1.y)
-        {
-            let x = va.0.x;
-            let (x1, x2) = if h_intersect.0.x < h_intersect.1.x {
-                (h_intersect.0.x, h_intersect.1.x)
-            } else {
-                (h_intersect.1.x, h_intersect.0.x)
-            };
-
-            if !(x == x2 && x >= x1) {
-                return false;
-            }
-        }
-
-        if within_range_exclusive(hori_y, va.0.y, va.1.y)
-            && within_range_inclusive(vb.0.x, h_intersect.0.x, h_intersect.1.y)
-        {
-            let x = va.0.x;
-            let (x1, x2) = if h_intersect.0.x < h_intersect.1.x {
-                (h_intersect.0.x, h_intersect.1.x)
-            } else {
-                (h_intersect.1.x, h_intersect.0.x)
-            };
-
-            if !(x == x2 && x <= x1) {
-                return false;
-            }
-        }
-    }
-
-    true
-}
-
 fn generate_rects(h: &[(IVec2, IVec2)], v: &[(IVec2, IVec2)]) -> Vec<(IVec2, IVec2)> {
     let mut rects = vec![];
 
@@ -220,9 +110,24 @@ fn generate_rects(h: &[(IVec2, IVec2)], v: &[(IVec2, IVec2)]) -> Vec<(IVec2, IVe
     rects
 }
 
+fn check_corners((a, b): &(IVec2, IVec2), rects: &[(IVec2, IVec2)]) -> bool {
+    let a = a.clone();
+    let b = b.clone();
+    let c = IVec2::new(a.x, b.y);
+    let d = IVec2::new(b.x, a.y);
+
+    let corners = [a, b, c, d];
+
+    corners.iter().all(|p| inside_rects(p, &rects))
+}
+
+fn check_edges(rect: &(IVec2, IVec2), rects: &[(IVec2, IVec2)]) -> bool {
+    
+}
+
 fn challenge() -> (i64, i64) {
     let input = utility::input::get_input(2025, 9).unwrap();
-    let numbers = input.lines().map(parse_vec2).collect::<Vec<_>>();
+    let numbers = TEST.lines().map(parse_vec2).collect::<Vec<_>>();
 
     let answer1 = {
         let mut max = 0;
@@ -240,42 +145,21 @@ fn challenge() -> (i64, i64) {
         let mut max = 0;
         let (h, v) = polygon_lines(&numbers);
         let rects = generate_rects(&h, &v);
-
-        let valid = rects.iter().all(|(p1, p2)| p1.x <= p2.x && p1.y <= p2.y);
-        assert!(valid);
-
-        assert!(rect_no_edge_out(
-            &(IVec2::new(2, 3), IVec2::new(9, 5)),
-            &h,
-            &v
-        ));
+        println!("{}", rects.len());
 
         for n in &numbers {
             for m in &numbers {
-                let a = *n;
-                let b = *m;
-                let c = IVec2::new(a.x, b.y);
-                let d = IVec2::new(b.x, a.y);
+                let rect = (
+                    IVec2::new(n.x.min(m.x), n.y.min(m.y)),
+                    IVec2::new(n.x.max(m.x), n.y.max(m.y)),
+                );
 
-                let corners = [a, b, c, d];
-
-                if corners.iter().all(|p| inside_rects(p, &rects)) {
-                    let rect = (
-                        IVec2::new(a.x.min(b.x), a.y.min(b.y)),
-                        IVec2::new(a.x.max(b.x), a.y.max(b.y)),
-                    );
-
-                    let x = (n.x - m.x).abs() + 1;
-                    let y = (n.y - m.y).abs() + 1;
+                if check_corners(&rect, &rects) {
+                    let x = (rect.1.x - rect.0.x) + 1;
+                    let y = (rect.1.y - rect.0.y)  + 1;
+                    assert!(x > 0 && y > 0);
                     let area = x * y;
-
-                    //dbg!(&rect);
-                    //dbg!(area);
-
-                    if rect_no_edge_out(&rect, &h, &v) {
-                        println!("PASSED!");
-                        max = area.max(max);
-                    }
+                    max = area.max(max);
                 }
             }
         }
